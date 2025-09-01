e6bb43993034fb7698920972e7ab4d5d07ccfaee
hjin9473 <hjin9473@gmail.com>
2025-09-01 14:40:40 +0900

    9/1

---
+++ js/main.js
@@ -0,0 +1,68 @@
+// ScrollMagic 사용
+// 그 외 scrollreveal
+const spyEls = document.querySelectorAll('section.scroll-spy');
+
+// init controller
+const controller = new ScrollMagic.Controller();
+
+spyEls.forEach(function (spyEl) {
+  // create a scene
+  new ScrollMagic.Scene({ // 감시할 장면 추가 및 옵션 지정
+    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: 0.5 // 화면의 50% 지점에서 보여짐 여부 감시(0~1사이 지정)
+  })
+  .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+  .addTo(controller); // 컨트롤러에 장면을 할당(필수!)
+});
+
+// Swiper 사용
+const swiper = new Swiper('.project .swiper', {
+  // 슬라이드 옵션 지정
+  direction: 'horizontal', // 수평 슬라이드
+  loop: true, // 반복 재생 여부, 1 -> 2 -> 3 -> 다시 1
+  // autoplay: { // 자동 재생 여부
+  //   delay: 5000 // 5초마다 슬라이드 바뀜
+  // },
+
+  // 페이지네이션 옵션
+  pagination: {
+    el: '.project .swiper-pagination',
+    clickable: true // 사용자의 페이지네이션 요소 제어 가능 여부
+  },
+
+  // 이전/다음 슬라이드 버튼 옵션
+  navigation: {
+    nextEl: '.project .swiper-button-next',
+    prevEl: '.project .swiper-button-prev',
+  },
+});
+
+// 모달창 띄우기
+const modal = document.querySelector('#modal');
+const modalBtn = document.querySelector('.project .btn-modal');
+const closeBtn = document.querySelector('#modal .btn-close');
+
+const imageModal = document.querySelector('#imageModal');
+const imageModalBtnList = document.querySelectorAll('.project .btn-modal-image');
+const imageCloseBtn = document.querySelector('#imageModal .btn-close');
+const imageEl = document.querySelector('#imageModal img');
+
+// Quiz: modalBtn 누르면 모달창이 뜨고 closeBtn 누르면 닫히도록 만들기
+modalBtn.addEventListener('click', function () {
+  modal.style.display = 'flex';
+  // modal.classList.add('show');
+});
+closeBtn.addEventListener('click', function () {
+  modal.style.display = 'none';
+  // modal.classList.remove('show');
+});
+
+imageModalBtnList.forEach(function (imageModalBtn) {
+  imageModalBtn.addEventListener('click', function () {
+    imageEl.src = imageModalBtn.dataset.imageSrc;
+    imageModal.style.display = 'flex';
+  });
+});
+imageCloseBtn.addEventListener('click', function () {
+  imageModal.style.display = 'none';
+});
